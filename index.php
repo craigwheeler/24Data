a50afc4a12e904445e61e6dcb9ea42ea4bc7f4e5
“Craig <craig@craigwheeler.net>
2018-04-08 12:24:09 -0400

    created template parts for header, content and footer

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php get_header(); ?>
+<?php
+wp_head();
+
+get_template_part('dist/template-parts/main', 'header');
+get_template_part('dist/template-parts/main', 'content');
+get_template_part('dist/template-parts/main', 'footer');
+
+wp_footer();
+?>
