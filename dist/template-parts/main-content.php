7682836b2d7812d39c06fa815d9aa64df6f7d30a
“Craig <craig@craigwheeler.net>
2018-04-08 15:10:31 -0400

    completed frontend layout structure and styles

--- dist/template-parts/main-content.php
+++ dist/template-parts/main-content.php
@@ -1,6 +1,35 @@
-<section class="container-fluid main-content">
-	<div class="col-sm-offset-4 col-sm-4">
-		<h1>content</h1>
+<section class="container main-content">
+	<div class="col-sm-offset-1 col-sm-6 icon">
+		<img class="img-responsive" src="wp-content/themes/24data/dist/img/FrontEndSkillsAssessmentIcon.png" alt="icon image">
+		<h1>This Icon Has Been Minified</h1>
+	</div>
+	<div class="col-sm-4 form-section">
+		<h1>This is the Form</h1>
+		<form>
+			<div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Your First Name *">
+			</div>
+			<div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Your Last Name *">
+			</div>
+			<div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Your Zip Code *">
+			</div>
+			<div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Contact Phone *">
+			</div>
+			<div class="form-group">
+				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Your Email *">
+			</div>
+			<div class="checkbox">
+				<label class="disclaimer">
+					<input type="checkbox"> I hereby consent to receive emails, text messages and other electronic communications at the telephone and email listed above.
+				</label>
+			</div>
+			<div class="col-sm-offset-3 col-sm-6">
+				<button type="submit" class="btn btn-default form-submit-btn">Submit</button>
+			</div>
+		</form>
 	</div>
 </section>
 
--- dist/template-parts/main-header.php
+++ dist/template-parts/main-header.php
@@ -1,4 +1,4 @@
-<header class="container-fluid main-footer">
+<header class="container-fluid main-header">
 	<div class="col-sm-offset-4 col-sm-4">
 		<h1>Front End Skills Test</h1>
 	</div>
