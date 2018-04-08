e748ca98310d9f3ffa37feb56f26f924fe776099
“Craig <craig@craigwheeler.net>
2018-04-08 18:03:07 -0400

    added js form validation

--- dist/template-parts/main-content.php
+++ dist/template-parts/main-content.php
@@ -5,29 +5,35 @@
 	</div>
 	<div class="col-sm-4 form-section">
 		<h1>This is the Form</h1>
-		<form>
-			<div class="form-group">
+		<form name="leadForm" action="submit.php" method="post">
+			<div class="form-group" id="firstName">
 				<input type="text" class="form-control" id="inputFirstName" placeholder="Your First Name *" maxlength="20" required>
+				<p id="error-message-first-name"></p>
 			</div>
-			<div class="form-group">
+			<div class="form-group" id="lastName">
 				<input type="text" class="form-control" id="inputLastName" placeholder="Your Last Name *" maxlength="20" required>
+				<p id="error-message-last-name"></p>
 			</div>
-			<div class="form-group">
+			<div class="form-group" id="zipCode">
 				<input type="text" class="form-control" id="inputZipCode" placeholder="Your Zip Code *" pattern="[0-9]{5}" required>
+				<p id="error-message-zipcode"></p>
 			</div>
-			<div class="form-group">
+			<div class="form-group" id="phone">
 				<input type="tel" class="form-control" id="inputPhone" placeholder="Contact Phone *" pattern="^[0-9\-\+\s\(\)]*$" required>
+				<p id="error-message-phone"></p>
 			</div>
-			<div class="form-group">
+			<div class="form-group" id="email">
 				<input type="email" class="form-control" id="inputEmail" placeholder="Your Email *" required>
+				<p id="error-message-email"></p>
 			</div>
 			<div class="checkbox">
-				<label class="disclaimer">
+				<label class="disclaimer" id="checkbox">
 					<input type="checkbox" required> I hereby consent to receive emails, text messages and other electronic communications at the telephone and email listed above.
+					<p id="error-message=checkbox"></p>
 				</label>
 			</div>
 			<div class="col-sm-offset-3 col-sm-6">
-				<button type="submit" class="btn btn-default form-submit-btn">Submit</button>
+				<button type="submit" class="btn btn-default form-submit-btn" onclick="validateForm()">Submit</button>
 			</div>
 		</form>
 	</div>
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,62 @@
-jQuery( document ).ready(function() {
-// this is just an empty annonymous doc ready function in jquery
-});
+
+// validate form data
+function validateForm() {
+    // variables
+    var inputFirstName = document.getElementById("inputFirstName");
+    var firstName = document.getElementById("firstName");
+
+    var inputLastName = document.getElementById("inputLastName");
+    var lastName = document.getElementById("lastName");
+
+    var inputZipCode = document.getElementById("inputZipCode");
+    var zipCode = document.getElementById("zipCode");
+
+    var inputPhone = document.getElementById("inputPhone");
+    var phone = document.getElementById("phone");
+
+    var inputEmail = document.getElementById("inputEmail");
+    var email = document.getElementById("email");
+
+
+    // functions
+    if (!inputFirstName.checkValidity()) {
+        document.getElementById("error-message-first-name").innerHTML = "Please enter your first name up to 20 characters";
+        firstName.classList.add("has-error");
+    } else if (inputFirstName.checkValidity()) {
+        firstName.classList.remove("has-error");
+        document.getElementById("error-message-first-name").style.display = "none";
+    }
+
+    if (!inputLastName.checkValidity()) {
+        document.getElementById("error-message-last-name").innerHTML = "Please enter your last name up to 20 characters";
+        lastName.classList.add("has-error");
+    } else if (inputLastName.checkValidity()) {
+        lastName.classList.remove("has-error");
+        document.getElementById("error-message-last-name").style.display = "none";
+    }
+
+    if (!inputZipCode.checkValidity()) {
+        document.getElementById("error-message-zipcode").innerHTML = "Please enter your zip code";
+        zipCode.classList.add("has-error");
+    } else if (inputZipCode.checkValidity()) {
+        zipCode.classList.remove("has-error");
+        document.getElementById("error-message-zipcode").style.display = "none";
+    }
+
+    if (!inputPhone.checkValidity()) {
+        document.getElementById("error-message-phone").innerHTML = "Required telephone format (xxx) xxx-xxxx";
+        phone.classList.add("has-error");
+    } else if (inputPhone.checkValidity()) {
+        phone.classList.remove("has-error");
+        document.getElementById("error-message-phone").style.display = "none";
+    }
+
+    if (!inputEmail.checkValidity()) {
+        document.getElementById("error-message-email").innerHTML = "Required email address";
+        email.classList.add("has-error");
+    } else if (inputEmail.checkValidity()) {
+        email.classList.remove("has-error");
+        document.getElementById("error-message-email").style.display = "none";
+    }
+
+}
