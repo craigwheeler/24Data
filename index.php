4aac1a672a8d582b6da1ab325d8eb0ae554e404b
“Craig <craig@craigwheeler.net>
2018-04-10 21:49:45 -0400

    added viewport meta tag to header

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<?php
-wp_head();
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>24 Data Skills Assessment</title>
+  <?php wp_head(); ?>
+</head>
+<body>
 
+<?php
 get_template_part('dist/template-parts/main', 'header');
 get_template_part('dist/template-parts/main', 'content');
 get_template_part('dist/template-parts/main', 'footer');
-
-wp_footer();
 ?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
